2c3e7e9745dfab403793c99283441161ce0c78ed
David Prieto <deybitsds1@gmail.com>
2024-05-06 23:06:43 -0500

    implementado vander en r: falta lagrange, grafico, bit reverso y cambiar colores

--- mul.py
+++ mul.py
@@ -2,6 +2,7 @@ import time
 from numpy.fft import fft, ifft
 import numpy as np
 
+# ---------- Operar
 def operar(funcion, pila_coeficientes):
 
     resultado = pila_coeficientes[0]
@@ -22,17 +23,97 @@ def operar(funcion, pila_coeficientes):
 def operar_dos_en_dos(funcion, lista_coef_1, lista_coef_2):
     return funcion(lista_coef_1, lista_coef_2)
 
+# ---------- Lagrange
 def mul_con_lagrange():
     pass
 
-def mul_con_vandermonde_r():
-    pass
+# ---------- Vandermonde en R
+def mul_con_vandermonde_r(coef_poli1, coef_poli2):
+    coef_poli1, coef_poli2 = IgualarPolinomios(coef_poli1, coef_poli2)
+    # Generar matrices de vandermonde en R
+    matriz_poli1 = matriz_vandermonde(generar_valores(len(coef_poli1)))
+    matriz_poli2 = matriz_vandermonde(generar_valores(len(coef_poli2)))
 
+    ev_pol1 = np.dot(matriz_poli1, coef_poli1)
+    ev_pol2 = np.dot(matriz_poli2, coef_poli2)
+
+    # Multiplicacion punto
+    producto_punto = ev_pol1 * ev_pol2
+
+    # Interpolación
+    inversa_vandermonde = np.linalg.inv(matriz_poli1)
+    resultado = np.dot(inversa_vandermonde, producto_punto)
+    ResultadoFinal = np.round(resultado).astype(int)
+    return ResultadoFinal.tolist()
+
+def completar_con_ceros(lista):
+    longitud_actual = len(lista)
+    siguiente_potencia_dos = 1
+    while siguiente_potencia_dos < longitud_actual:
+        siguiente_potencia_dos *= 2
+    
+    # Si la longitud actual ya es una potencia de 2, no hay necesidad de completar con ceros
+    if longitud_actual == siguiente_potencia_dos:
+        return lista  
+    # Calcular cuántos ceros se deben agregar
+    ceros_a_agregar = siguiente_potencia_dos - longitud_actual
+    lista_completa = lista + [0] * ceros_a_agregar
+    return lista_completa
+
+def matriz_vandermonde(valores):
+    n = len(valores)
+    matriz = np.zeros((n, n))
+
+    for i in range(n):
+        for j in range(n):
+            matriz[i][j] = valores[i]**j
+
+    return matriz
+
+def generar_valores(n):
+    l = []
+    for k in range(n):
+        l.append(k)
+    return l
 
 # ---------- Vandermonde en I
-def mul_con_vandermonde_i():
-    pass
+def mul_con_vandermonde_i(a,b):
+    # Obtener el tamaño de la entrada
+    a,b = IgualarPolinomios(a, b)
+    N = len(a)
 
+    # Calcular la matriz Vandermonde
+    w_n = np.exp(2j * np.pi / N)
+    V_n = np.vander(np.array([w_n ** i for i in range(N)]), increasing=True)
+    # Evaluacion de coeficientes
+    y1 = np.dot(V_n, a)
+    y1_redondeado = [round(x.real, 10) + round(x.imag, 10) * 1j for x in y1]
+    y2 = np.dot(V_n, b)
+    y2_redondeado = [round(x.real, 10) + round(x.imag, 10) * 1j for x in y2]
+    # Multiplicación punto (Yk)
+    ProductoPunto = [x * y for x, y in zip(y1_redondeado, y2_redondeado)]
+    np.set_printoptions(precision=3, suppress=True)
+
+    # Interpolación
+    # Inversa de la matriz de vandermonde 
+    V_n_inv = np.array([[1/x for x in row] for row in V_n])
+    # Vn^-1 . Yk / N
+    ResultadoFinal = np.dot(V_n_inv, ProductoPunto)
+    ResultadoFinal = ResultadoFinal / len(a)
+    ResultadoFinal = np.round(ResultadoFinal).astype(int)
+    return ResultadoFinal.tolist()
+
+def IgualarPolinomios(a, b):
+    GradoResultante = len(a) + len(b)  # Grado del polinomio resultante
+    TamanoFinal = GradoResultante - 1  # Tamaño deseado de las listas
+
+    # Añadir ceros al final de cada lista para igualar su tamaño al tamaño deseado
+    a += [0] * (TamanoFinal - len(a))
+    b += [0] * (TamanoFinal - len(b))
+
+    return a, b
+
+# ---------- Bit reverso
 def mul_con_bit_reverso():
     pass
 
@@ -70,7 +151,6 @@ def IgualarPolinomios(a, b):
 
     return a, b
 
-
 def VandermondeI(a,b):
     # Obtener el tamaño de la entrada
     a,b = IgualarPolinomios(a, b)
@@ -96,3 +176,17 @@ def VandermondeI(a,b):
     ResultadoFinal = ResultadoFinal / len(a)
     ResultadoFinal = np.round(ResultadoFinal).astype(int)
     return ResultadoFinal.tolist()
+
+def completar_con_ceros(lista):
+    longitud_actual = len(lista)
+    siguiente_potencia_dos = 1
+    while siguiente_potencia_dos < longitud_actual:
+        siguiente_potencia_dos *= 2
+    
+    # Si la longitud actual ya es una potencia de 2, no hay necesidad de completar con ceros
+    if longitud_actual == siguiente_potencia_dos:
+        return lista  
+    # Calcular cuántos ceros se deben agregar
+    ceros_a_agregar = siguiente_potencia_dos - longitud_actual
+    lista_completa = lista + [0] * ceros_a_agregar
+    return lista_completa
--- polinomios.py
+++ polinomios.py
@@ -2,7 +2,21 @@ def recuperar_coeficientes_polinomio_mal_escrito(string):
     pass
 
 def imprimir_polinomio_bien_escrito(lista_coeficientes):
-    pass 
+    grado = len(lista_coeficientes) - 1
+    polinomio = ""
+
+    for i, coeficiente in reversed(list(enumerate(lista_coeficientes))):
+        if coeficiente != 0:
+            if i == 0:
+                polinomio += f"{coeficiente}"
+            elif i == 1:
+                polinomio += f"{coeficiente}x"
+            else:
+                polinomio += f"{coeficiente}x^{i}"
+
+            if i != 0:
+                polinomio += " + "
+    return polinomio
 
 def polinomio_es_valido(string):
     return #True
